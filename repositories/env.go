3d92a52e36f810cc9fbb5fb23d533ed4f7de61c3
Fadli <mnurfadd@gmail.com>
2021-03-16 22:52:20 +0700

    Added redis connection

--- repositories/env.go
+++ repositories/env.go
@@ -3,15 +3,36 @@ package repositories
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gomodule/redigo/redis"
 	"log"
+	"os"
 )
 
-func Connect() *sql.DB {
+func Connect() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "root:secret@tcp(localhost:3306)/stockbit")
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return db
+	return db, nil
+}
+
+var pool *redis.Pool
+
+func ConnectRedis(maxConnection int) *redis.Pool {
+	pool = &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: maxConnection,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", "localhost:6379")
+			if err != nil {
+				log.Printf("ERROR: fail init redis pool: %s", err.Error())
+				os.Exit(1)
+			}
+			return conn, err
+		},
+	}
+
+	return pool
 }
