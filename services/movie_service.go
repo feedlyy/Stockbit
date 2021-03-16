ad7dc335f5e364ce97f740d27ff8342770b02f66
Fadli <mnurfadd@gmail.com>
2021-03-16 22:49:28 +0700

    Update with clean-architecture

--- services/movie_service.go
+++ services/movie_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"Stockbit/models"
 	"Stockbit/repositories"
 	"encoding/json"
 	"log"
@@ -17,24 +18,43 @@ import (
 //	Movies allMovies.AllMoviesRepository
 //}
 
-func AllMovies (w http.ResponseWriter, r *http.Request)  {
-	Movies, err, url := repositories.GetMovies(r)
-	if err != nil {
-		log.Println(err)
-	}
-	err = repositories.Store(url)
-	if err != nil {
-		log.Println(err)
-	}
-	_ = json.NewEncoder(w).Encode(Movies)
+type MovieService interface {
+	AllMovies (r *http.Request) *models.Result
+	DetailMovie (w http.ResponseWriter, r *http.Request)
 }
 
-func DetailMovie (w http.ResponseWriter, r *http.Request)  {
-	Movie, err, url := repositories.GetMovie(r)
+type movieService struct {
+	repoMovie repositories.AllMoviesRepository
+	repoDB repositories.LogRepository
+	repoMov repositories.MovieRepository
+}
+
+func NewMovieService (a repositories.AllMoviesRepository, b repositories.LogRepository) *movieService {
+	return &movieService{
+		repoMovie: a,
+		repoDB: b,
+	}
+}
+
+func (ms movieService) AllMovies (r *http.Request) *models.Result {
+	res, err, url := ms.repoMovie.GetMovies(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ms.repoDB.Store(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
+
+func (ms movieService) DetailMovie (w http.ResponseWriter, r *http.Request)  {
+	Movie, err, url := ms.repoMov.GetMovie(r)
 	if err != nil {
 		log.Println(err)
 	}
-	err = repositories.Store(url)
+	err = ms.repoDB.Store(url)
 	if err != nil {
 		log.Println(err)
 	}
